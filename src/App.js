ff93a9abda085fd7d794e3eff83f27a315163487
Irene Wang <ireneziwang@gmail.com>
2022-09-06 17:02:24 -0500

    trying to make my App.js file show

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+export default class App extends React.Component {
+  render () {
+    return (
     <div className="App">
-    <h1>Hi, I'm Irene</h1>
+      <h1>Hi, I'm Irene</h1>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
 
